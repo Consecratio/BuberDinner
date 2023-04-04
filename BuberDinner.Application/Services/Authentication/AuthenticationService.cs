9acb36a63c5e6a59954beb6ad73626529f2d8c48
Consecratio <jcasique@gmail.com>
2023-04-03 17:48:52 -0700

    Add JWT Authentication Infrastructure
    - Added code to generate JWT in Infrastructure

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,15 +1,33 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        // TODO: Check if user already exists
+
+        // TODO: Create user (generate unique ID)
+
+        // TODO: Create JWT token
+        Guid userId = Guid.NewGuid();
+
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
         return new AuthenticationResult(
             Guid.NewGuid(),
             firstName,
             lastName,
             email,
-            "token"
+            token
         );
     }
 
